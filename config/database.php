604bf088b5f6ebac7d42f55140347dff4a830c40
aarti321 <aartibhattarai8@gmail.com>
2024-08-13 17:00:53 +1000

    install mongodb

--- config/database.php
+++ config/database.php
@@ -109,7 +109,20 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'connections' => [
+
+    'mongodb' => [
+        'driver'   => 'mongodb',
+        'host'     => env('DB_HOST', '127.0.0.1'),
+        'port'     => env('DB_PORT', 27017),
+        'database' => env('DB_DATABASE', 'mydatabase'),
+        'username' => env('DB_USERNAME', ''),
+        'password' => env('DB_PASSWORD', ''),
+        'options'  => [
+            'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
+        ],
     ],
+
 
     /*
     |--------------------------------------------------------------------------
