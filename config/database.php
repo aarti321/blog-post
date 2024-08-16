089cbe61ed717c47cbabecff661010b0f694b277
aarti321 <aartibhattarai8@gmail.com>
2024-08-17 00:57:35 +1000

    fix issuse

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
             'driver'   => 'mongodb',
             'host'     => env('DB_HOST', '127.0.0.1'),
             'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'blogproject'),
+            'database' => env('DB_DATABASE', 'blog'),
             'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'options'  => [
